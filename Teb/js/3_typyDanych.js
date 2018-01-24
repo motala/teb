c579999142c689708c13176dfa56a62ee1f60208
motala <mateusz-motala@wp.pl>
2018-01-24 08:54:26 +0100

    typyDanych

---
+++ Teb/js/3_typyDanych.js
@@ -0,0 +1,59 @@
+var a = 10, b = "5a";
+var wynik = a  + b;
+
+//alert(wynik);
+//alert(a + b);
+console.log("tekst");//10
+console.log(a);
+console.log(typeof(a)); //number
+console.log(typeof(b)); //string
+console.log(typeof(wynik)); //string
+
+console.trace();
+
+var _zmienna1 = 10;
+
+//var 3liczba =10; //błędny nazwa zmiennej
+
+var zmiennaZmiennoPrzecinkowa =2.5;
+console.log(zmiennaZmiennoPrzecinkowa);
+console.log(typeof(zmiennaZmiennoPrzecinkowa));
+
+//bool
+
+var prawda = true;
+console.log(typeof(prawda)); //boolean
+
+//string
+
+var tekst = "Janusz";
+console.log(typeof(tekst)); //string
+
+//undefined
+
+var inna;
+console.log(inna); //undefined
+console.log(typeof(inna)); //undefined
+
+//object
+
+var tab =["aqua", "brązowy"];
+console.log(tab);
+console.log(typeof(tab));//object
+
+//###############################################################################################################################################################################
+
+//var liczba = prompt("Podaj liczbę");
+var liczba1 = prompt("Podaj liczbę", "liczba 1");
+var liczba2 = prompt("Podaj liczbę", "liczba 2");
+var suma = liczba1 + liczba2; //konkatenacja
+console.log(suma);
+
+liczba1 = parseInt(liczba1);
+liczba2 = parseInt(liczba2);
+var suma1 = liczba1 + liczba2;
+console.log("Po Konwersji typów" + suma1);
+
+console.log(typeof(liczba1)); //number
+console.log(typeof(liczba2)); //number
+
