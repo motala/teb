fd401e6011f965efdcab9589611e8e8e3abec499
motala <mateusz-motala@wp.pl>
2018-02-06 15:19:17 +0100

    math

--- Teb/js/8_Math.js
+++ Teb/js/8_Math.js
@@ -1,6 +1,7 @@
 var pi = Math.PI;
-console.log (pi);
+console.log(pi);
 
+//################################################################################################################################################################################
 //zadanie 1
 /*Uzytkownik podaje z klawiatury promień koła.
 Oblicz pole i wyswietl w formacie : "Pole koła wynosi : ......... cm2".*/
@@ -10,13 +11,15 @@ var elPrzycisk = document.getElementById("przycisk");
 var elKomunikat = document.getElementById("komunikat");
 var pole;
 var dlugosc;
+
 function poleKola(){
-    if(elPromień.value >0){
+    if(elPromień.value > 0){
     pole = pi*Math.pow(elPromień.value,2);
     pole = pole.toFixed(2);
-    elKomunikat.innerHTML = "Pole koła wynosi:" + pole + "cm <sup>2</sup>";
+    elKomunikat.innerHTML = "Pole koła wynosi:" + pole + "cm<sup>2</sup>";
         }else{
             elKomunikat.innerHTML = "Promień koła musi być liczbą naturalną.";
         }
 }
 elPrzycisk.addEventListener('click',poleKola);
+//################################################################################################################################################################################
