ad5a0939cc03147e11b6ee8dfe89c6e551d16b07
motala <mateusz-motala@wp.pl>
2018-02-27 13:09:32 +0100

    Petle while

---
+++ Teb/js/9_1_Petle.js
@@ -0,0 +1,10 @@
+//uzytkownik podaje z klawiatury dwie wartosci petla bedzie wykonywala sie tak dlugo dopuki x  nie bedzie wieksze od y
+
+var a=0 , b=0
+
+while(a >= b){
+    a=prompt('Podaj a');
+    b=prompt('Podaj b');
+}
+console.log(a=+a);
+console.log(b=+b);
--- Teb/js/9_Petle.js
+++ Teb/js/9_Petle.js
@@ -119,3 +119,15 @@ ilosc = elWiersz.value;
 }
 elPrzycisk.addEventListener('click',znak1);
 
+
+
+//uzytkownik podaje z klawiatury dwie wartosci petla bedzie wykonywala sie tak dlugo dopuki x  nie bedzie wieksze od y
+
+var a=0 , b=0
+
+while(a >= b){
+    a=prompt('Podaj a');
+    b=prompt('Podaj b');
+}
+console.log(a=+a);
+console.log(b=+b);
