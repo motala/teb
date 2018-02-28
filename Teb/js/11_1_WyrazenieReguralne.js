31dd8f55f1f99745642635ea7a47f523171332bb
motala <mateusz-motala@wp.pl>
2018-02-28 10:27:36 +0100

    zadanie domowe

---
+++ Teb/js/11_1_WyrazenieReguralne.js
@@ -0,0 +1,8 @@
+/*//zadanie 1
+uzytkownik podaje w polu tekstowym kod kodPocztowy
+Pod pole wyswietl komunikat czy dane został pradidłowo wprowadzone: ................
+jeślio będą prawidłowe dane to wyświetl komunikat o tresci: "Prawidłowy kod pocztowy: ......................."
+jeśli będą błędny  to wyświetl komunikat "błędny kod pocztowy" i umieść kursor w polu tekstowym*/
+
+/*zadanie 2
+sprawdz adres poczty elektronicznej*/
--- Teb/js/11_WyrazenieReguralne.js
+++ Teb/js/11_WyrazenieReguralne.js
@@ -1,6 +1,6 @@
 //true or false (prawda lub fałsz)
 
-var tekst = 'sad@';
+var tekst = 'testtest'
 var regula = /janusz/;
 regula = /janusz/i;
 regula = /^janusz/i;
@@ -30,9 +30,79 @@ regula = /[a-z]\S/;
 
 
 //mail
-
+//+ jeden lub więcej znaków
 regula= /[a-z]+@/;
+//? jeden lub brak znaku
+regula = /^mąk[a]?@/;
+
+
+regula = /[a]{1}/;
+regula = /[a]{2}/;
+regula = /[a]{2,4}/;
+regula = /[a]{2,}/;
+regula = /[a-z]{2,6}/;
+regula = /^[a-z]{2,}$/;
+
+regula = /\//;
+regula = /\s/;//wymaga spacji
+regula = /\S/;//brak spacji
+regula = /./;//jeden znak
+regula = /../;//dwa znaki
+regula = /.../;//trzy znaki
+regula = /\./;//kropka
+regula = /\$/;//dolar
+regula = /\.[a-z]{2,3}$/; //np. .pl .com .edu
+
+// \w znak będący literą, cyfrą, znakiem podkreślenia
+// \W znak nie będący literą, cyfrą, znakiem podkreślenia
+
+regula = /\w/;
+regula = /\W/;
+
+
+// \d znak bedący cyfrą
+// \D znak nie bedący cyfrą
+
+regula = /\d/;
+regula = /\D/;
+
+//grupa
+
+regula = /(test)/;
+regula = /(test){2}/;
+
+
+//kod pocztowy
+var kod = '12-231'
+var kodPocztowy = /^\d{2}-\d{3}$/;
+
+
+
+//**********************************************************************************************
+
+var muzyka = " tiesto.mp3 plik@mp3 arkusz.csv szkola.mp3 tekst.mp3"
+
+//var mp3 = /\w\.mp3$/;
+var mp3 = /\w+\.mp3/g;
+var test = mp3.test(muzyka);
+var testMatch = muzyka.match(mp3);
+var iloscUtworow = testMatch.length;
+
+
+document.write('<ol>');
+for(var i=0; i<iloscUtworow; i++){
+    document.write('<li>'+testMatch[i]+ '</li>');
+}
+document.write('</ol>');
+
+
+
 
 
 var sprawdz = regula.test(tekst);
+var sprawdzKod = kodPocztowy.test(kod);
 console.log(sprawdz);
+console.log(sprawdzKod);
+console.log(test);
+console.log(testMatch);
+console.log('Ilość utworów: '+ iloscUtworow);
