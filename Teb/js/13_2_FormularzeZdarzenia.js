cb031ab336fa2939e5b05fc608412b8a972a36b2
motala <mateusz-motala@wp.pl>
2018-03-13 16:18:35 +0100

    zdarzenia

--- Teb/js/13_2_FormularzeZdarzenia.js
+++ Teb/js/13_2_FormularzeZdarzenia.js
@@ -1,8 +1,8 @@
 var elImie = document.getElementById('imie');
+var elNazwisko = document.getElementById('nazwisko');
 var elBlad = document.getElementById('blad');
 var elKolor = document.getElementById('kolor');
-var elNazwisko = document.getElementById('nazwisko');
-var elBlokImie = document.getElementById('blokImie');
+var BlokImie = document.getElementById('blokImie');
 var elBlokNazwisko = document.getElementById('blokNazwisko');
 function sprawdz(){
     if(this.value.length < 2){
@@ -24,10 +24,24 @@ function sprawdz(){
 
 
 function zmienImie(){
+   if(elImie.length !=''){
+    BlokImie.style.color= elKolor.value;
+    BlokImie.innerHTML = elImie.value;
+
+   }
 
 }
 
+
+function zmienKolor(){
+   elBlokNazwisko.style.color =elKolor.value;
+    }
+
+
+
 elImie.addEventListener('blur',sprawdz);
 elNazwisko.addEventListener('blur',sprawdz);
+BlokImie.addEventListener('mouseover',zmienImie);
+elKolor.addEventListener('change',zmienKolor);
 /*window.addEventListener('load',witaj);
 window.addEventListener('unload',wyjscie);*/
