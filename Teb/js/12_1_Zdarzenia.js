d6e8d609d12a192da5eae9b39df5c680b88e31b9
motala <mateusz-motala@wp.pl>
2018-03-14 08:47:44 +0100

    zdarzenie

--- Teb/js/12_1_Zdarzenia.js
+++ Teb/js/12_1_Zdarzenia.js
@@ -1,8 +1,33 @@
 var elLitera = document.getElementById('litera');
-var elKod=document.getElementById('kod');
+var elKod=document.getElementById('Kod');
 
 function litera(){
-console.log(event.keyCode);
+//console.log(event.keyCode);
+    let Kod = event.keyCode;
+    switch(Kod){
+        case 32:
+            Kod = 'spacja';
+            break;
+        case 8:
+            Kod = 'backspace';
+            break;
+        case 16:
+            Kod = 'shift';
+            break;
+        case 17:
+            Kod = 'ctrl';
+            break;
+        case 18:
+            Kod = 'alt';
+            break;
+        case 20:
+            Kod = 'CAPSLOCK';
+            break;
+        case 27:
+            Kod = 'esc';
+            break;
+    }
+    elKod.innerHTML = "Kod znaku : " + Kod;
 }
 
 elLitera.addEventListener('keyup', litera);
