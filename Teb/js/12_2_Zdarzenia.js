23c84ed671ef78560c0049f85ae7b3bde8598545
motala <mateusz-motala@wp.pl>
2018-03-14 10:19:15 +0100

    zdarzenia2

--- Teb/js/12_2_Zdarzenia.js
+++ Teb/js/12_2_Zdarzenia.js
@@ -1,7 +1,31 @@
- var elSzer =document.getElementById('szerokosc');
+var elSzer =document.getElementById('szerokosc');
 var elWys = document.getElementById('wysokosc');
 var elBlok = document.getElementById('blok');
+var elKolor = document.getElementById('kolor');
+var elLewo = document.getElementById('lewo');
+var elDodajLewo=document.getElementById('dodajLewo');
 
-elBlok.style.backgroundColor = 'red';
+elBlok.style.backgroundColor = elKolor.value;
 elBlok.style.height = '0px';
 elBlok.style.width = '0px';
+
+function rozmiar(){
+    let sz = elSzer.value;
+    let w = elWys.value;
+    elBlok.style.width = sz + 'px';
+    elBlok.style.height= w + 'px';
+}
+
+function kolor(){
+    elBlok.style.backgroundColor = elKolor.value;
+}
+
+function dl(){
+   elBlok.offsetLeft=elBlok.offsetLeft + 10 + 'px';
+}
+elLewo.value= elBlok.offsetLeft;
+
+elSzer.addEventListener('change',rozmiar);
+elWys.addEventListener('change',rozmiar);
+elKolor.addEventListener('change',kolor);
+elDodajLewo.addEventListener('click',dl);
